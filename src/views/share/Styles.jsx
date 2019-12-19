542ecd171dce8d88f294c1c7681522de0d9479cd
Tolu Adesina <tadesina90@gmail.com>
2019-12-19 23:57:01 +0100

    remove the style components and move to styles

--- src/views/share/Styles.jsx
+++ src/views/share/Styles.jsx
@@ -4,11 +4,13 @@ export const DrawerContainer = styled.div`
   .theDrawer {
     background-color: white;
     border-radius: 1rem 1rem 0 0;
-    padding: 1rem;
+    padding: 1rem 2rem;
     position: fixed;
     width: 100%;
     height: 20vh;
     display: flex;
+    justify-content: space-evenly;
+    align-items: center;
     bottom: 0;
     transform: translate3d(0, 100%, 0);
     transition: transform 0.5s ease;
@@ -51,4 +53,45 @@ export const DrawerContainer = styled.div`
       color: white;
     }
   }
+`;
+
+export const Container = styled.div`
+  background-color: ${props => props.theme};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-width: 310px;
+  min-height: 550px;
+  padding: 1rem;
+`;
+export const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+export const Title = styled.h2`
+  font-family: "GTSuperText";
+`;
+export const Name = styled.div`
+  font-size: 16px;
+`;
+export const Footer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 0.8rem;
+`;
+export const Slogan = styled.h2`
+  font-size: 24px;
+  font-family: "GTSuperText";
+`;
+export const Span = styled.span`
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+  font-size: 10px;
+  line-height: 12px;
+  padding: 0.25rem 1rem;
+  border-radius: 8px;
 `;
