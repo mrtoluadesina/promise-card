392f79594ddf5a70c3267bd63382a154ca14992d
Tolu Adesina <tadesina90@gmail.com>
2019-12-19 14:16:01 +0100

    create styles.js to handle the styled components for the components folder

---
+++ src/components/Styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Table = styled.div`
+  border: 1px solid #111111;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const Header = styled.div`
+  border-bottom: 1px solid #111111;
+  background-color: #f9f9f9;
+`;
+
+export const List = styled.ul`
+  list-style: none;
+`;
