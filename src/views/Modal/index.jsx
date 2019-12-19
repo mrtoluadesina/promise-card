b78f7e95efc0367af97ec543543823ed7c920d4f
Tolu Adesina <tadesina90@gmail.com>
2019-12-19 18:30:37 +0100

    update modal view with complete markup

---
+++ src/views/Modal/index.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./index.scss";
+
+export default function Modal(props) {
+  const { handleChange } = props;
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <h2>Your Promise Card is Almost Ready</h2>
+        <form>
+          <div className="form-group">
+            <label>Give your card a title</label>
+            <input
+              type="text"
+              name="Title"
+              placeholder="E.g. Christmas, Detty December, Birthday"
+              onClick={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Full Name</label>
+            <input
+              type="text"
+              name="fullname"
+              placeholder="Tolu Adesina"
+              onClick={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>
+              Email Address <br/><small>To receive your promisecard via mail</small>
+            </label>
+            <input
+              type="text"
+              name="email"
+              placeholder="example@mail.com"
+              onClick={handleChange}
+            />
+          </div>
+          <input type="submit" className="btn" value="See My Promise Card" />
+        </form>
+      </div>
+    </div>
+  );
+}
