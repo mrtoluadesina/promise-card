18ff622c610fd5c9994efa1ecfe6fda9f3ad8ec4
Tolu Adesina <tadesina90@gmail.com>
2019-12-19 14:17:32 +0100

    update markup with content for the homepage

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { ThemeContext } from "../../store";
+
+import "./index.scss";
+import { Table } from "../../components/Styles";
 
 export default function Home() {
+  const [state, dispatch] = useContext(ThemeContext);
+
   return (
-    <div>Create your Wishlist</div>
-    );
+    <Container theme={state.currentTheme} className="main">
+      <div className="app-content">
+        <Heading>Create your Wishlist</Heading>
+        <Small>Choose color</Small>
+        <Table />
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  background-color: ${props => props.theme};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Heading = styled.h2`
+  font-size: 27px;
+  width: 200px;
+  line-height: 35px;
+  text-align: center;
+`;
+
+const Small = styled.h3`
+  font-size: 16px;
+`;
