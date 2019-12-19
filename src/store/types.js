6cf5686d382e73ce189e4b3512fac995939ce076
josemak25 <amakirij@gmail.com>
2019-12-19 17:55:50 +0100

    add update form type for input fields

--- src/store/types.js
+++ src/store/types.js
@@ -3,3 +3,6 @@
 export const YELLOW_THEME = "YELLOW_THEME";
 export const PINK_THEME = "PINK_THEME";
 export const BLUE_THEME = "BLUE_THEME";
+
+// update form details
+export const UPDATE_FORM = "UPDATE_FORM";
