8f142c27b226b04796591f9a6dadfa61195736a5
josemak25 <amakirij@gmail.com>
2019-12-19 12:49:13 +0100

    add app theme to home container

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { ThemeContext } from "../../store";
 
 export default function Home() {
+  const [state, dispatch] = useContext(ThemeContext);
+
   return (
-    <div>Create your Wishlist</div>
-    );
+    <Container theme={state.currentTheme} className="main">
+      <div>Create your Wishlist</div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  background-color: ${props => props.theme};
+`;
