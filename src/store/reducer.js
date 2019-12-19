5d90128eb1af8d74a35591101bb4290d3aa58059
josemak25 <amakirij@gmail.com>
2019-12-19 14:38:38 +0100

    add active bubble to theme reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,11 +3,11 @@ import { YELLOW_THEME, PINK_THEME, BLUE_THEME } from "./types";
 export default function themeReducer(state, action) {
   switch (action.type) {
     case YELLOW_THEME:
-      return { ...state, currentTheme: state.yellow };
+      return { ...state, currentTheme: state.yellow, active: action.active };
     case PINK_THEME:
-      return { ...state, currentTheme: state.pink };
+      return { ...state, currentTheme: state.pink, active: action.active };
     case BLUE_THEME:
-      return { ...state, currentTheme: state.blue };
+      return { ...state, currentTheme: state.blue, active: action.active };
     default:
       return state;
   }
