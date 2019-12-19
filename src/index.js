5398316d99c8c3d9fd834738b27d1a4dbcd18d86
Tolu Adesina <tadesina90@gmail.com>
2019-12-19 14:15:22 +0100

    update on imported font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './fonts/GTSuperText-Regular.otf';
+import './fonts/GTSuperText-Book.otf';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
