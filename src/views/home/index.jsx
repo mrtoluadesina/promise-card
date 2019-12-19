6215ed586b6c1b09e6cf4d69a7b2b3d174a24bf6
josemak25 <amakirij@gmail.com>
2019-12-19 14:51:37 +0100

    Merge branch 'develop' of https://github.com/mrtoluadesina/promise-card into develop

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -3,37 +3,45 @@ import styled from "styled-components";
 import { ThemeContext } from "../../store";
 import { YELLOW_THEME, PINK_THEME, BLUE_THEME } from "../../store/types";
 import { setActiveStyle } from "../../utils";
-import "./home.scss";
+import "./index.scss";
+
+import { Table } from "../../components/Styles";
 
 export default function Home() {
   const [state, dispatch] = useContext(ThemeContext);
 
   return (
     <Container theme={state.currentTheme} className="main">
-      <h1>Create your Wishlist</h1>
-      <p>Chose color</p>
-      <div className="choose_colors">
-        <span
-          onClick={() => dispatch({ type: YELLOW_THEME, active: YELLOW_THEME })}
-          style={setActiveStyle({
-            active: state.active,
-            boxType: YELLOW_THEME
-          })}
-        ></span>
-        <span
-          onClick={() => dispatch({ type: PINK_THEME, active: PINK_THEME })}
-          style={setActiveStyle({
-            active: state.active,
-            boxType: PINK_THEME
-          })}
-        ></span>
-        <span
-          onClick={() => dispatch({ type: BLUE_THEME, active: BLUE_THEME })}
-          style={setActiveStyle({
-            active: state.active,
-            boxType: BLUE_THEME
-          })}
-        ></span>
+      <div className="app-content">
+        <h2 className="header">Create your Wishlist</h2>
+        <h3 className="small_header">Choose color</h3>
+        <div className="choose_colors">
+          <span
+            onClick={() =>
+              dispatch({ type: YELLOW_THEME, active: YELLOW_THEME })
+            }
+            style={setActiveStyle({
+              active: state.active,
+              boxType: YELLOW_THEME
+            })}
+          ></span>
+          <span
+            onClick={() => dispatch({ type: PINK_THEME, active: PINK_THEME })}
+            style={setActiveStyle({
+              active: state.active,
+              boxType: PINK_THEME
+            })}
+          ></span>
+          <span
+            onClick={() => dispatch({ type: BLUE_THEME, active: BLUE_THEME })}
+            style={setActiveStyle({
+              active: state.active,
+              boxType: BLUE_THEME
+            })}
+          ></span>
+        </div>
+
+        <Table />
       </div>
     </Container>
   );
@@ -41,4 +49,8 @@ export default function Home() {
 
 const Container = styled.div`
   background-color: ${props => props.theme};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
